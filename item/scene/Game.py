019d8f951c00628eae7f5c84364775597f31a239
unknown <januar.evan2012@gmail.com>
2023-06-16 12:00:02 +0700

    add : Close Button to Main Menu

--- item/scene/Game.py
+++ item/scene/Game.py
@@ -2,6 +2,7 @@ import pygame as p
 from item.chess.Board import Board
 from item.core.GameObject import GameObject
 from item.ui.popup.NotificationFinished import NotificationFinished
+from item.ui.TextButton import TextButton
 
 class Game(GameObject):
     def __init__(self):
@@ -12,10 +13,13 @@ class Game(GameObject):
         self.on_destroy += self.__destroy
         self.on_update += self.__update
         self.on_keyboard_down += self.__on_keyboard_down
+        self.close_button = None
 
     def __awake(self):
         self.board = Board()
         self.instantiate(self.board)
+        self.close_button = self.__create_close_button("X", 0)
+        self.close_button.on_mouse_down += lambda : self.load_scene(0)
 
     def __destroy(self, game_object : GameObject):
         self.board = None
@@ -27,4 +31,17 @@ class Game(GameObject):
 
     def __on_keyboard_down(self, event : p.event.Event):
         if event.key == p.K_ESCAPE:
-            self.load_scene(0)
+            self.load_scene(0)\
+            
+    def __create_close_button(self, text : str, top_margin : int) -> TextButton:
+        button = self.instantiate(TextButton(
+            p.Rect(0, 0, 280, 100),
+            p.Color(255, 255, 255, 50),
+            8,
+            text = text, 
+            text_size = 48
+        ), self)
+        button.set_anchor((0.5, 0))
+        button.set_pivot((0.5, 0))
+        button.set_margin(top = top_margin)
+        return button
